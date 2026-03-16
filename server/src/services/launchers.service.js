b72509de653c35d5f086bfd9c28a89dcf9f94272
itamar levi <leviitamar034@gmail.com>
2026-03-16 11:01:52 +0200

    truble with git

--- server/src/services/launchers.service.js
+++
@@ -1,8 +0,0 @@
-import { getDB } from "../db/mongo";
-import { ObjectId } from "mongodb";
-
-export async function createLauncher({ name, cit, rocketType, latitude, longitude }) {
-    const db = getDB()
-    const launchers = db.collection('launchers')
-    
-}   
